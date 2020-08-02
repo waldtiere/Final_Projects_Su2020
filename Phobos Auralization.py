60b978c5a4241bae5097b5899e72a5fc33d91a76
waldtiere <@BoyZ2MeN2>
2020-08-02 15:16:30 -0500

    Updated the main call, create_dict_from_file, and plots_and_waves so that each time the funtion is called, a more reasonable set of data plots and audio files are rendered and so that a user can set their own delimiters appropriate to the file they extract from the Horizons database.
    toDo:
    
    run again with a different dataset.
    
    Create Value Errors in Create-Dictionary-From File that lets a user know that their column names do not match delimeters or vice versa.
    
    Write Doc tests

--- Phobos Auralization.py
+++ Phobos Auralization.py
@@ -22,7 +22,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wave, struct, math, random, matplotlib
 
-def create_dict_from_file(filename, delimeters, first_char):
+def create_dict_from_file(filename, delimeters, first_char, column_names):
     """This function takes in a filename of an output file from the Horizons On-line
     Ephemeris System v4.70 (Solar System Dynamics Group Jet Propulsion Laboratory
     Pasadena, CA, USA) and creates a list of dictionaries from it with the relevant information
@@ -48,9 +48,9 @@ def create_dict_from_file(filename, delimeters, first_char):
                 # This initializes a new measurement dictionary
                 key = measurement_current_line[0]
                 new_measurement_dictionary = {
-                    "X": '0',
-                    "Y": '0',
-                    "Z": '0',
+                    column_names[0]: '0',
+                    column_names[1]: '0',
+                    column_names[2]: '0',
                 }
                 print(measurement_current_line)
                 # this determines if a line starts with 'X', splits it at the X =,Y =,Z = indicators
@@ -60,9 +60,9 @@ def create_dict_from_file(filename, delimeters, first_char):
                 measurement_current_line = re.split(delimeters, line.strip(' '))
                 print(measurement_current_line)
                 if len(measurement_current_line) == 4:
-                    new_measurement_dictionary['X'] = float(measurement_current_line[1].strip())
-                    new_measurement_dictionary['Y'] = float(measurement_current_line[2].strip())
-                    new_measurement_dictionary['Z'] = float(measurement_current_line[3].strip())
+                    new_measurement_dictionary[column_names[0]] = float(measurement_current_line[1].strip())
+                    new_measurement_dictionary[column_names[1]] = float(measurement_current_line[2].strip())
+                    new_measurement_dictionary[column_names[2]] = float(measurement_current_line[3].strip())
                     measurements_file_container[key] = new_measurement_dictionary
                     print(new_measurement_dictionary)
                 # this stops the processing when the end of data key '$$EOE' is reached
@@ -201,7 +201,7 @@ def find_zero_points(DataSeries, column):
 
 # This function calls all other functions given a specific waveform.
 
-def plots_and_waves(datafile, identifier, data_inc=500):
+def plots_and_waves(datafile, identifier, delimiters, first_char, column_names, data_inc=500):
     """
     This function takes a text file as output from NASA's Horizon Database and outpus a number of plots
     .wav files that auralize different aspets of the dataset as sound and plots it as a graph.
@@ -211,133 +211,99 @@ def plots_and_waves(datafile, identifier, data_inc=500):
     :return: various plots and soundfiles
     """
     # create a separate dictionary with X,Y,Z postion data
-    XYZ_measurements = create_dict_from_file(datafile,'X =|Y =|Z =','X')
+    XYZ_measurements = create_dict_from_file(datafile, delimiters, first_char, column_names)
     data_length = len(XYZ_measurements)
     data_inc = data_length/data_inc
     print(data_length)
 
-    # create a separate dictionary with VX,VY,VZ velocity data
-    XYZ_velocities = create_dict_from_file(datafile,'VX=|VY=|VZ=','V')
 
-
-    #create dataframe out of XYZ position data dictionary
+    #create dataframe out of XYZ data dictionary
     measurement_DataFrame = convert_measuerment_dict_to_DataFrame(XYZ_measurements)
 
-    #create dataframe out of velocity data and rename it with correct labels
-    velocity_DataFrame = convert_measuerment_dict_to_DataFrame(XYZ_velocities)
-    velocity_DataFrame = velocity_DataFrame.rename(columns={'X': 'VX', 'Y': 'VY', 'Z': 'VZ'})
 
     # Plot movement of the X,Y, and Z coordniates
-    print('Position Measurements \n', measurement_DataFrame, '\n')
-    plot_data(measurement_DataFrame, '{0} Position'.format(identifier), 0, data_length, ['X','Y','Z'])
-
-    # plot changes in the X,Y, and Z velocities
-    print('Velocity Measurements \n', velocity_DataFrame, '\n')
-    plot_data(velocity_DataFrame, '{0} Velocity'.format(identifier), 0, data_length, ['VX','VY','VZ'])
+    print('Raw Data \n', measurement_DataFrame, '\n')
+    plot_data(measurement_DataFrame, 'image/{0} Raw'.format(identifier), 0, data_length, column_names)
 
     # convert the coordinate position data into amplitude data and plot
-    amp_position = (convert_coordinates_to_amplitude(measurement_DataFrame))
-    print(amp_position)
-    plot_data(amp_position, '{0} Positon as Amplitude'.format(identifier), 0, data_length, ['X','Y','Z'])
+    amp = (convert_coordinates_to_amplitude(measurement_DataFrame))
+    print(amp)
+    plot_data(amp, 'image/{0} as Amplitude'.format(identifier), 0, data_length, column_names)
 
     # separate out the converted X,Y, and Z coordniate position
     # amplitude datasets and use to generate soundfiles
-    amp_pos_X = amp_position['X']
-    amp_pos_Y = amp_position['Y']
-    amp_pos_Z = amp_position['Z']
-    make_waves(amp_pos_X, "{0} amp_pos_X.wav".format(identifier))
-    make_waves(amp_pos_Y, "{0} amp_pos_Y.wav".format(identifier))
-    make_waves(amp_pos_Z, "{0} amp_pos_Z.wav".format(identifier))
+    amp_1 = amp[column_names[0]]
+    amp_2 = amp[column_names[1]]
+    amp_3 = amp[column_names[2]]
+    make_waves(amp_1, "sound/{0}_1.wav".format(identifier))
+    make_waves(amp_2, "sound/{0}_2.wav".format(identifier))
+    make_waves(amp_3, "sound/{0}_3.wav".format(identifier))
 
-    # convert the velocity data into amplitude data and plot
-    amp_velocity = (convert_coordinates_to_amplitude(velocity_DataFrame))
-    print(amp_velocity)
-    plot_data(amp_velocity, "{0} Velocity as Amplitude".format(identifier), 0, data_length, ['VX','VY','VZ'])
-
-    # separate out the converted X,Y, and Z velocity
-    # amplitude datasets and use to generate soundfiles
-    amp_vel_X = amp_velocity['VX']
-    amp_vel_Y = amp_velocity['VY']
-    amp_vel_Z = amp_velocity['VZ']
-    make_waves(amp_vel_X, "{0} amp_vel_X.wav".format(identifier))
-    make_waves(amp_vel_Y, "{0} amp_vel_Y.wav".format(identifier))
-    make_waves(amp_vel_Z, "{0} amp_vel_Z.wav".format(identifier))
-
-    # calculate the mean of all 3 X, Y, Z to create a new column for the mean
+    # calculate the mean of all 3 rows of the dataframe to create a new column for the mean
     # velocity and add it to the dataframe
-    mean_wav = get_mean_wav(amp_velocity)
-    amp_velocity['VM'] = mean_wav
-
+    mean_wav = get_mean_wav(amp)
+    amp['M'] = mean_wav
+    print(column_names + ['M'])
     # plot the full DataFrame with X, Y, Z and Mean Velocities as well as 3 closer samples
-    plot_data(amp_velocity, '{0} Mean Velocity as Amplitude'.format(identifier), 0, data_length, ['VX', 'VY', 'VZ', 'VM'])
-    plot_data(amp_velocity, '{0} Mean Velocity as Amplitude Sample 1'.format(identifier), 0, int(data_length/data_inc), ['VX', 'VY', 'VZ', 'VM'])
-    plot_data(amp_velocity, '{0} Mean Velocity as Amplitude Sample 2'.format(identifier), int(data_length/3), int((data_length/3)+(data_length/data_inc)), ['VX', 'VY', 'VZ', 'VM'])
-    plot_data(amp_velocity, '{0} Mean Velocity as Amplitude Sample 3'.format(identifier), int((data_length/3)*2), int(((data_length/3)*2)+(data_length/data_inc)), ['VX', 'VY', 'VZ', 'VM'])
+    plot_data(amp, 'image/{0} Mean as Amplitude'.format(identifier), 0, data_length, (column_names + ['M']))
+    plot_data(amp, 'image/{0} Mean as Amplitude Sample 1'.format(identifier), 0, int(data_length/data_inc), (column_names + ['M']))
+    plot_data(amp, 'image/{0} Mean as Amplitude Sample 2'.format(identifier), int(data_length/3), int((data_length/3)+(data_length/data_inc)), (column_names + ['M']))
+    plot_data(amp, 'image/{0} Mean as Amplitude Sample 3'.format(identifier), int((data_length/3)*2), int(((data_length/3)*2)+(data_length/data_inc)), (column_names + ['M']))
 
-    # re-normalize amplitude values of amp_velocity and re-plot
-    amp_vel_M = convert_coordinates_to_amplitude(amp_velocity)
-    plot_data(amp_vel_M, '{0}-Mean Velocity as Amplitude Normalized'.format(identifier), 0, data_length, ['VX', 'VY', 'VZ', 'VM'])
+    # re-normalize amplitude values of amp and re-plot
+    amp_M = convert_coordinates_to_amplitude(amp)
+    plot_data(amp_M, 'image/{0}-Mean as Amplitude Normalized'.format(identifier), 0, data_length, (column_names + ['M']))
 
-    # create sound from the "mean" wavefile as well as 3 the mean of 3 different samples
-    ps1 = 0
-    pe1 = int(data_length/data_inc)
-    ps2 = int(data_length/3)
-    pe2 = int((data_length/3)+(data_length/data_inc))
-    ps3 = int((data_length/3)*2)
-    pe3 = int(((data_length/3)*2)+(data_length/data_inc))
-
-    make_waves(amp_vel_M['VM'], "{0} amp_vel_M.wav".format(identifier))
-    make_waves(amp_vel_M['VM'][ps1:pe1], "{0} amp_vel_M_0-100.wav".format(identifier), 1000)
-    make_waves(amp_vel_M['VM'][ps2:pe2], "{0} amp_vel_M_4k-4_1K.wav".format(identifier), 1000)
-    make_waves(amp_vel_M['VM'][ps3:pe3], "{0} amp_vel_M_10k-10_1k.wav".format(identifier), 1000)
+    # create sound from the "mean" wavefile
+    make_waves(amp_M['M'], "sound/{0}_mean.wav".format(identifier))
 
     # sample out the first full wave cycle in order to create a waveform that begins and ends on a zero point
     # define zero points of all 4 X, Y, Z and Mean Velocities, using the the
     # first and 3rd to ientify one "cycle" of the wave from zero through positive
     # phase space, back through zero to negative phase space, and back to zero.
-    zero_points_X = find_zero_points(amp_vel_M, 'VX')
+    zero_points_X = find_zero_points(amp_M, column_names[0])
     zpx1 = zero_points_X[0]
     zpx2 = zero_points_X[1]
-    zero_points_Y = find_zero_points(amp_vel_M, 'VY')
+    zero_points_Y = find_zero_points(amp_M, column_names[1])
     zpy1 = zero_points_Y[0]
     zpy2 = zero_points_Y[1]
-    zero_points_Z = find_zero_points(amp_vel_M, 'VZ')
+    zero_points_Z = find_zero_points(amp_M, column_names[2])
     zpz1 = zero_points_Z[0]
     zpz2 = zero_points_Z[1]
-    zero_points_M = find_zero_points(amp_vel_M, 'VM')
+    zero_points_M = find_zero_points(amp_M, 'M')
     zpm1 = zero_points_M[0]
     zpm2 = zero_points_M[1]
 
     # plot the four sampled out full waveforms
-    plot_data(amp_vel_M['VX'][zpx1:zpx2], '{0}-Mean Velocity as Amplitude Normalized One Cycle VX'.format(identifier),
-              0, data_length, ['VX'])
-    plot_data(amp_vel_M['VY'][zpy1:zpy2], '{0}-Mean Velocity as Amplitude Normalized One Cycle VY'.format(identifier),
-              0, data_length, ['VY'])
-    plot_data(amp_vel_M['VZ'][zpz1:zpz2], '{0}-Mean Velocity as Amplitude Normalized One Cycle VZ'.format(identifier),
-              0, data_length, ['VZ'])
-    plot_data(amp_vel_M['VM'][zpm1:zpm2], '{0}-Mean Velocity as Amplitude Normalized One Cycle VM'.format(identifier),
-              0, data_length, ['VM'])
+    plot_data(amp_M[column_names[0]][zpx1:zpx2], 'image/{0}-Mean as Amplitude Normalized One Cycle_1'.format(identifier),
+              0, data_length, (column_names[0]))
+    plot_data(amp_M[column_names[1]][zpy1:zpy2], 'image/{0}-Mean as Amplitude Normalized One Cycle_2'.format(identifier),
+              0, data_length, (column_names[1]))
+    plot_data(amp_M[column_names[2]][zpz1:zpz2], 'image/{0}-Mean as Amplitude Normalized One Cycle_3'.format(identifier),
+              0, data_length, (column_names[2]))
+    plot_data(amp_M['M'][zpm1:zpm2], 'image/{0}-Mean as Amplitude Normalized One Cycle_mean'.format(identifier),
+              0, data_length, 'M')
 
     # create a continuous repeated tone out of each sampled wave
-    make_waves(amp_vel_M['VX'][zpx1:zpx2], "{0} 1period_amp_vel_X.wav".format(identifier), 50)
-    make_waves(amp_vel_M['VY'][zpy1:zpy2], "{0} 1period_amp_vel_Y.wav".format(identifier), 50)
-    make_waves(amp_vel_M['VZ'][zpz1:zpz2], "{0} 1period_amp_vel_Z.wav".format(identifier), 50)
-    make_waves(amp_vel_M['VM'][zpm1:zpm2], "{0} 1period_amp_vel_M.wav".format(identifier), 50)
+    make_waves(amp_M[column_names[0]][zpx1:zpx2], "sound/{0} 1cycle_1.wav".format(identifier), 50)
+    make_waves(amp_M[column_names[1]][zpy1:zpy2], "sound/{0} 1cycle_2.wav".format(identifier), 50)
+    make_waves(amp_M[column_names[2]][zpz1:zpz2], "sound/{0} 1cycle_3.wav".format(identifier), 50)
+    make_waves(amp_M['M'][zpm1:zpm2], "sound/{0} 1cycle_mean.wav".format(identifier), 50)
 
     # create a continuous repeated tone out of each sampled wave with higher pitch
-    make_waves(amp_vel_M['VX'][zpx1:zpx2:5], "{0} 1period_amp_vel_X_higher.wav".format(identifier), 500)
-    make_waves(amp_vel_M['VY'][zpy1:zpy2:5], "{0} 1period_amp_vel_Y_higher.wav".format(identifier), 500)
-    make_waves(amp_vel_M['VZ'][zpz1:zpz2:5], "{0} 1period_amp_vel_Z_higher.wav".format(identifier), 500)
-    make_waves(amp_vel_M['VM'][zpm1:zpm2:5], "{0} 1period_amp_vel_M_higher.wav".format(identifier), 500)
+    make_waves(amp_M[column_names[0]][zpx1:zpx2:5], "sound/{0} 1cycle_1_higher.wav".format(identifier), 500)
+    make_waves(amp_M[column_names[1]][zpy1:zpy2:5], "sound/{0} 1cycle_2_higher.wav".format(identifier), 500)
+    make_waves(amp_M[column_names[2]][zpz1:zpz2:5], "sound/{0} 1cycle_3_higher.wav".format(identifier), 500)
+    make_waves(amp_M['M'][zpm1:zpm2:5], "sound/{0} 1cycle_mean_higher.wav".format(identifier), 500)
 
     # create a continuous repeated tone out of each sampled wave with highest pitch
-    make_waves(amp_vel_M['VX'][zpx1:zpx2:30], "{0} 1period_amp_vel_X_highest.wav".format(identifier), 5000)
-    make_waves(amp_vel_M['VY'][zpy1:zpy2:30], "{0} 1period_amp_vel_Y_highest.wav".format(identifier), 5000)
-    make_waves(amp_vel_M['VZ'][zpz1:zpz2:30], "{0} 1period_amp_vel_Z_highest.wav".format(identifier), 5000)
-    make_waves(amp_vel_M['VM'][zpm1:zpm2:30], "{0} 1period_amp_vel_M_highest.wav".format(identifier), 5000)
+    make_waves(amp_M[column_names[0]][zpx1:zpx2:30], "sound/{0} 1cycle_1_highest.wav".format(identifier), 5000)
+    make_waves(amp_M[column_names[1]][zpy1:zpy2:30], "sound/{0} 1cycle_2_highest.wav".format(identifier), 5000)
+    make_waves(amp_M[column_names[2]][zpz1:zpz2:30], "sound/{0} 1cycle_3_highest.wav".format(identifier), 5000)
+    make_waves(amp_M['M'][zpm1:zpm2:30], "sound/{0} 1cycle_mean_highest.wav".format(identifier), 5000)
 
 
 
 # call main function
 
-plots_and_waves('ParkerSolarProbe.txt', 'Parker', 2000)
+plots_and_waves('ParkerSolarProbe.txt', 'Parker Velocity', 'VX=|VY=|VZ=', 'V', ['VX', 'VY', 'VZ'], 2000)
